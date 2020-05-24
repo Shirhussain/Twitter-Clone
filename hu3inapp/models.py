d76049461de3c9ec0a4923fe73f5d54aaec0c06a
Hu3in <sh.danishyar@gmail.com>
2020-05-24 15:55:22 +0430

    models, forms and signal are done

--- hu3inapp/models.py
+++ hu3inapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    content = models.CharField(max_length=240)
+    timestamp = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content[:5]
+
+
+    @property
+    def number_of_comments(self):
+        Comment.objects.filter(post_connected=self).count()
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_connected = models.ForeignKey(Post, on_delete = models.CASCADE)        
